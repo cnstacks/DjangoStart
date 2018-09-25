39f2287acd577f60f0fbf6cd30f6a2d8c54047d3
tqtl911@163.com <19930911cXS>
2018-09-25 08:10:19 +0800

    删除冗余代码。

--- message/views.py
+++ message/views.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
 
 # Create your views here.
+def getform(request):
+    return render(request, 'message_form.html', )
