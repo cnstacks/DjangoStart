dfb65e2b27f546477f7250a8d9457a3bbc930320
tqtl911@163.com <19930911cXS>
2018-09-25 09:09:41 +0800

    Django中其他的字段类型：

--- message/models.py
+++ message/models.py
@@ -4,5 +4,21 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-
 # Create your models here.
+class UserMessage(models.Model):
+    name = models.CharField(max_length=32, verbose_name='用户名')
+    email = models.EmailField(verbose_name='邮箱')
+    address = models.CharField(max_length=100, verbose_name=u'联系地址')
+    message = models.CharField(max_length=500, verbose_name=u'留言信息')
+    """
+    Django中其他的字段类型：
+    models.ForeignKey
+    models.DateTimeField
+    models.IntegerField
+    models.IPAddressField
+    models.FileField
+    models.ImageField
+    """
+
+    class Meta:
+        verbose_name = u'用户留言信息'
