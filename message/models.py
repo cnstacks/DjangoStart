3fd1abbd4ccd8461132bf71c639cda85b12df437
tqtl911@163.com <19930911cXS>
2018-09-25 09:15:38 +0800

    verbose_name = u'用户留言信息'         verbose_name_plural =verbose_name
            # db_table = "user_message"  # 指定生成的表名；
            # ordering = "-object_id"#指定排序；
            # ordering = "object_id"#加上-号，反排序

--- message/models.py
+++ message/models.py
@@ -6,7 +6,8 @@ from django.db import models
 
 # Create your models here.
 class UserMessage(models.Model):
-    name = models.CharField(max_length=32, verbose_name='用户名')
+    object_id = models.CharField(max_length=50, default="", primary_key=True, verbose_name=u"主键")
+    name = models.CharField(max_length=32, null=True, blank=True, default="", verbose_name='用户名')
     email = models.EmailField(verbose_name='邮箱')
     address = models.CharField(max_length=100, verbose_name=u'联系地址')
     message = models.CharField(max_length=500, verbose_name=u'留言信息')
@@ -22,3 +23,8 @@ class UserMessage(models.Model):
 
     class Meta:
         verbose_name = u'用户留言信息'
+        verbose_name_plural = verbose_name
+
+        # db_table = "user_message"
+        # ordering = "-object_id"
+        # ordering = "object_id"
